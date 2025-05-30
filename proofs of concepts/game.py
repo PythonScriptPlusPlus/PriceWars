e9ce8bf4f33b0c9cb12994f1cb7bfb4d41ad6d8b
George <gsemmail@gmail.com>
2025-05-30 14:00:08 +0300

    game prototype in text

---
+++ proofs of concepts/game.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+#demand function: p = 100 - Q
+#TC_i = Costs[-1]*q_i
+
+money = 1000
+Costs = [50]
+overall_production = 0
+
+year = 1
+run = True
+while run:
+    print(f'Year {year}')
+    print(f'Money: {money}')
+    print('demand function: p = 100 - Q')
+    if year == 1:
+        print(f'costs: {Costs[-1]}')
+    else:
+        print(f'your employees learnt something, costs are now {Costs[-1]}')
+    
+    q_i = int(input('Input how much to produce: '))
+    
+    Q = q_i
+    overall_production += q_i
+    p = max(100 - Q, 0)
+    
+    profit = q_i * p - Costs[-1] * q_i
+    
+    money += profit
+    
+    print(f'Profit: {profit}')
+    
+    if money <= 0:
+        print('You are out of money!')
+        run = False
+    
+    if input('Continue? (y/n): ').lower() != 'y':
+        run = False
+    
+    alpha = -0.05
+    ut = np.random.normal(0, 0.025)
+    newCost = Costs[0] * overall_production**alpha * np.e**ut
+    Costs.append(round(newCost, 2))
+
+    year += 1
