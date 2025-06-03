17704cbbd57053a0f65a3fb13b9491b369f51c93
George <gsemmail@gmail.com>
2025-06-03 13:12:49 +0300

    made template for web version of the game

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="healthbar"></div>
+    <div class="info"></div>
+    <div class="leftbar"></div>
+    <div class="rightbar"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-};
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld,
+//   },
+// };
 </script>
+
+<style scoped lang="scss">
+.app {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  grid-template-rows: 50px 1fr;
+}
+
+.healthbar,
+.info,
+.leftbar,
+.rightbar {
+  box-sizing: border-box; // ensures border is inside the element
+  height: 100%;
+  width: 100%;
+}
+
+.healthbar {
+  grid-row: 1;
+  grid-column: 1;
+  border: 3px solid #ff0000;
+}
+
+.info {
+  grid-row: 1;
+  grid-column: 2;
+  border: 3px solid #00ff00;
+}
+
+.leftbar {
+  grid-row: 2;
+  grid-column: 1;
+  border: 3px solid #0000ff;
+}
+
+.rightbar {
+  grid-row: 2;
+  grid-column: 2;
+  border: 3px solid #ffff00;
+}
+</style>
