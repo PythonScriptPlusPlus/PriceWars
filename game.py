be227a681adf9a32e2d4d2fd7d0ca6d03ec9cc94
George <gsemmail@gmail.com>
2025-06-17 18:46:18 +0300

    comments in backend files

--- game.py
+++ game.py
@@ -1,33 +1,34 @@
 import numpy as np
 import math
 
+# Функция для решения квадратного уравнения ax^2 + bx + c = 0
 def solve_equation(a, b, c):
     discriminant = b**2 - 4*a*c
     if discriminant < 0:
-        print('no work',b**2, 4*a*c, discriminant)
         return None  # No real roots
     root1 = (-b + math.sqrt(discriminant)) / (2 * a)
     root2 = (-b - math.sqrt(discriminant)) / (2 * a)
-    print(f"Roots: {root1}, {root2}")
     return (root1, root2)
 
+# Класс Company представляет компанию с её финансами, производственными затратами и периодами
+# Этот класс используется и игроком, и противником
 class Company:
     def __init__(self, money, q, cost, period=1, demand=100, opponent=False, additional_cost=0, one_time_cost=0):
-        self.q = q
-        self.money = money
-        self.cost = cost
-        self.overall_production = 0
-        self.period = period
-        self.health = 100
-        self.demand = demand
-        self.opponent = opponent
-        self.additional_cost = additional_cost
-        self.one_time_cost = one_time_cost
+        self.q = q # Количество продукции, производимое компанией
+        self.money = money # Количество денег у компании
+        self.cost = cost # Стоимость производства компании
+        self.overall_production = 0 # Общее количество произведённой продукции
+        self.period = period # Количество периодов, которое компания существует
+        self.health = 100 # Здоровье компании, которое уменьшается при убытках. Репрезентует терпение инвесторов
+        self.demand = demand # Функция спроса, которая определяет цену в зависимости от количества продукции
+        self.opponent = opponent # Флаг, указывающий, является ли компания противником
+        self.additional_cost = additional_cost # Дополнительные затраты, которые компания несёт при производстве
+        self.one_time_cost = one_time_cost # Одноразовые затраты, которые компания несёт при входе на рынок
 
     # demand function: p = 100 - Q
     def produce(self, enemy=None):
         is_enemy_defeated = False
-        if not enemy:
+        if not enemy: # Производство при отсутствии противника на рынке
             profit = (self.demand - self.q) * self.q - self.cost[-1] * self.q - self.additional_cost - self.one_time_cost
             self.one_time_cost = 0
             self.money += profit
@@ -35,11 +36,11 @@ class Company:
             self.period += 1
             self.overall_production += self.q
 
-            optimal_q = (self.demand - self.cost[-1])/2
-            optimal_profit = (self.demand - optimal_q) * optimal_q - self.cost[-1] * optimal_q - self.additional_cost
-            exitLog = ''
+            optimal_q = (self.demand - self.cost[-1])/2 # Оптимальное количество продукции, которое нужно произвести для максимизации прибыли
+            optimal_profit = (self.demand - optimal_q) * optimal_q - self.cost[-1] * optimal_q - self.additional_cost # Оптимальная прибыль, которую можно получить при оптимальном количестве продукции. Используется для восстановления здоровья компании
+            exitLog = '' # Дополнительная информация о производстве, которая будет отображаться в интерфейсе. Испольуется, когда есть противник на рынке
         else:
-            if enemy.one_time_cost >= enemy.money:
+            if enemy.one_time_cost >= enemy.money: # Если противник не может войти на рынок из-за нехватки денег
                 exitLog = '<span style="color:#c22955">Конкурирующая компания не смогла войти на рынок, из-за высокой стоимости вхождения</span>\n'
                 is_enemy_defeated = True
                 self.produce()
@@ -56,6 +57,7 @@ class Company:
             enemy.one_time_cost = 0
             enemy.money += enemy_profit
 
+            # Противник теряет убытки, если его прибыль меньше или равна нулю
             if enemy.q == 0 or enemy_profit <= 0:
                 enemy.health -= 34
                 if enemy.health > 0:
@@ -68,24 +70,19 @@ class Company:
             optimal_q = min(solve_equation(1, 2*(self.cost[-1]-self.demand), self.demand**2 + self.cost[-1]**2 - 4*enemy.additional_cost - 2*self.cost[-1]*self.demand))
             optimal_profit = (self.demand - optimal_q) * optimal_q - self.cost[-1] * optimal_q
 
-            print(f"Your profit: {profit}, Your money: {self.money}, your optimal q: {optimal_q}, your optimal profit: {optimal_profit}")
-            print(f"Enemy profit: {enemy_profit}, Enemy money: {enemy.money}, Enemy q: {enemy.q}, Enemy period: {enemy.period}, enemy cost: {enemy.cost[-1]}")
-            print(f"Enemy TC: {enemy.additional_cost} + {enemy.cost[-1]} * q")
 
             self.health -= 10 * (enemy.period/4)**3
-            if 10 * (enemy.period/4)**3 > profit/optimal_profit * 25:
+            if 10 * (enemy.period/4)**3 > profit/optimal_profit * 25: # Игрок теряет здоровье, если оппонент присутствует на рынке достаточно долго и/или игрок не делает оптимальные действия
                 exitLog += '<span style="color:#c22955">Конкурирующая компания достаточно долго была на рынке, и ваши инвесторы начали присматриваться к ней</span>\n'
             
             if enemy.health <= 0:
-                print('Enemy has been defeated')
                 is_enemy_defeated = True
         if profit <= 0:
             self.health -= 25
         else:
-            print(f'supposed to heal, optimal_q: {optimal_q}, optimal profit: {optimal_profit}, ratio: {profit/optimal_profit}')
             self.health = round(min(100, self.health + profit/optimal_profit * 25))
 
-
+        # Формула с производства Генри Форда. Уменьшает затраты на производство, всзязи с обучением сотрудников и оптимизацией процессов
         alpha = -0.05
         ut = np.random.normal(0, 0.025)
         newCost = self.cost[0] * self.overall_production**alpha * np.e**ut
--- server.py
+++ server.py
@@ -11,35 +11,37 @@ CORS(app, resources={r"/*": {"origins": [
     "http://5.180.174.128",
     "http://5.180.174.128:8080"
 ]}})
-# Create a global player instance
-Player = Company(1000000, 0, [50])  # Initial values
-Opponent = None
-Opponent_window = 0
-demand_constant = 100
-base_cost = 50
-entry_cost = 0
-enemy_modifier = 1
-property_price = 1000
-player_plots = []
-opponent_adcost = 50
+# Создаёт начальные значения для игрока и противника
+Player = Company(1000000, 0, [50])  # Создаём игрока с начальными деньгами, количеством продукции и стоимостью
+Opponent = None # Изначально противник не существует
+Opponent_window = 0 # Счётчик периодов, между победой над противником и его появлением
+demand_constant = 100 # Начальная величина спроса
+base_cost = 50 # Начальная стоимость производства
+entry_cost = 0 # Стоимость входа на рынок для противника
+enemy_modifier = 1 # Модификатор для противника, увеличивается при победе над ним
+property_price = 1000 # Начальная цена собственности, которую игрок может купить
+player_plots = [] # Список собственности игрока, который будет обновляться при покупке
+opponent_adcost = 50 # Начальная стоимость производства для противника, которая будет увеличиваться при покупке собственности
 
-log = 'Игра запущена. Выберите количество продукции для производства.'
+log = 'Игра запущена. Выберите количество продукции для производства.' # История действий игрока, которая будет отображаться в Vue-приложении
 
+# Передаём начальные значения для игрока в Vue-приложение
 @app.route('/data', methods=['GET'])
 def get_data():
     data = {
         'health': Player.health,
         'period': Player.period,
-        'money': Player.money,  # Use Player's money
+        'money': Player.money,
         'demand': 100,
-        'costs': Player.cost[-1],  # Use Player's current cost
-        'overallProduction': Player.overall_production,  # Use Player's overall production
+        'costs': Player.cost[-1],
+        'overallProduction': Player.overall_production,
         'long_text': log,
         'property_price': property_price,
         'playersPlots': player_plots,
     }
     return jsonify(data)
 
+# Производит продукцию игрока и противника, если он существует
 @app.route('/production', methods=['POST'])
 def receive_production():
     global log, Opponent, Opponent_window, Player, demand_constant, enemy_modifier, property_price
@@ -51,16 +53,17 @@ def receive_production():
     try:
         Player.q = int(production_amount)
         if Player.period < 10:
-            line, is_enemy_defeated = Player.produce()
+            line, is_enemy_defeated = Player.produce() # Производим продукцию игрока без противника
         else:
             if Player.period == 10 or (Opponent_window == -1 and Player.period > 2):
                 Opponent = Company(1000*enemy_modifier, 0, [base_cost], period=1, demand=demand_constant, opponent=True, additional_cost=opponent_adcost, one_time_cost=entry_cost)
 
-            line, is_enemy_defeated = Player.produce(Opponent)
-        # print(Player.money)
+            line, is_enemy_defeated = Player.produce(Opponent) # Производим продукцию игрока с противником
+            
+        # Вывод информации о производстве
         long_text = 'В период <span style="color:skyblue">{}</span> ваша компания произвела <span style="color:#ffc55b">{}</span> единиц продукции'.format(Player.period-1, Player.q) + '\n' + long_text
         long_text = 'В период <span style="color:skyblue">{}</span> прибыль от произведённой продукции составила <span style="color:#39f139">{}</span> рублей'.format(Player.period-1,round(Player.q * (Player.demand - Player.q) - Player.cost[-2] * Player.q - Player.additional_cost,2)) + '\n' + long_text
-        if is_enemy_defeated:
+        if is_enemy_defeated: # Проверяем, победил ли игрок противника
             long_text = '<span style="color:green">Вы победили конкурента!</span>' + '\n' + line + long_text
             Opponent = None
             Opponent_window = random.randint(2,5)
@@ -77,12 +80,13 @@ def receive_production():
         if Player.period % 10 == 0 and Player.period > 10:
             demand_constant += 20
 
+        # Генерируем случайную цену собственности для следующей покупки
         property_price = round(np.random.normal(100, 5),2)
-        # print(Player.health)
+        
     except Exception as e:
         tb = traceback.format_exc()
         return jsonify({"status": "error on server", "message": str(e), "traceback": tb})
-    return jsonify({
+    return jsonify({ # Возвращаем данные, которые изменились после производства, обратно в Vue-приложение
         "status": "success",
         "received": production_amount,
         "money": Player.money,
@@ -93,10 +97,11 @@ def receive_production():
         "property_price": property_price,
     })
 
+# Перезагружает игру, сбрасывая все значения до начальных
 @app.route('/reset', methods=['POST'])
 def reset_game():
     global Player, log, Opponent, entry_cost, enemy_modifier, property_price, player_plots
-    Player = Company(1000000, 0, [50])  # Reset to default values
+    Player = Company(1000000, 0, [50])
     data = {
         'health': 100,
         'period': Player.period,
@@ -116,34 +121,38 @@ def reset_game():
     log = data['long_text']
     return jsonify(data)
 
+# Увеличивает стоимость производства на указанную величину для игрока и противника
 @app.route('/increase_cost', methods=['POST'])
 def increase_cost():
     global Player, base_cost
     data = request.get_json()
-    amount = int(data.get('amount', 0))
+    amount = int(data.get('amount', 0)) # Получаем величину увеличения стоимости производства из запроса
     base_cost += amount
     Player.cost = [c + amount for c in Player.cost]
     return jsonify({'costs': Player.cost[-1]})
 
+# Лоббирование игрока для входа на рынок
 @app.route('/invest', methods=['POST'])
 def invest():
     global Player, log, entry_cost
     data = request.get_json()
-    invested_money = data.get('investedMoney')
-    action = data.get('action')
+    invested_money = data.get('investedMoney') # Получаем сумму инвестиций из запроса
+    action = data.get('action') # Получаем действие из запроса
     is_invested = data.get('isInvested')
 
     if invested_money <= Player.money:
         Player.money -= invested_money
         is_invested = True
         entry_cost += action
-    # Example logic: just echo back some dummy values
-    response = {
+    
+    # Возвращаем последствия лоббирования игрока в Vue-приложение
+    response = { 
         "isInvested": is_invested,
         "newMoney": Player.money,
     }
     return jsonify(response)
 
+# Покупка собственности игроком
 @app.route('/buy_property', methods=['POST'])
 def buy_property():
     global Player, log, property_price, Opponent, opponent_adcost, base_cost
@@ -155,19 +164,21 @@ def buy_property():
         log = f'Вы приобрели собственность за {property_price} рублей.'
         player_plots.append(property_price)
         plots.append(property_price)
-        Player.additional_cost += property_price * property_taxation
-        Player.cost[-1] += round(property_price * property_taxation,2)
+        Player.additional_cost += property_price * property_taxation # Увеличиваем стоимость существования компании игрока на величину налога на собственность
+        Player.cost[-1] += round(property_price * property_taxation,2) # Увеличиваем стоимость производства игрока на величину налога на собственность
 
-        if Opponent:
+        if Opponent: # Если противник существует, увеличиваем его стоимость производства
             Opponent.additional_cost += property_price * property_taxation/2
             Opponent.cost[-1] += property_price * property_taxation
-        else:
+        else: # Если противник не существует, увеличиваем базовую стоимость производства
             opponent_adcost += property_price * property_taxation / 2
             base_cost += property_price * property_taxation / 2
 
+        # Генерируем случайную цену собственности для следующей покупки
         property_price = round(np.random.normal(1000, 5), 2)
     response = {
         "status": "success",
+
         "playersPlots": plots,
         "money": Player.money,
         "property_price": property_price,
